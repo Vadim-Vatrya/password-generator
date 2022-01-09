eee1de296416e1dcb64ac8ff86f102731210cc4b
Vadim-Vatrya <VatryaVV@gmail.com>
2022-01-09 22:33:17 +0200

    fix js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function generate() {
   let password = "";
 
-  let length = +document.getElementById("length");
-  let lowerCase = document.getElementById("lowercase");
-  let upperCase = document.getElementById("uppercase");
-  let symbols = document.getElementById("symbols");
-  let numbers = document.getElementById("numbers");
+  let length = +document.getElementById("length").value;
 
-  if (lowerCase + upperCase + symbols + number <= 0)
-    return;
+  let lowerCase = document.getElementById("lowercase").checked;
+  let upperCase = document.getElementById("uppercase").checked;
+  let symbols = document.getElementById("symbols").checked;
+  let numbers = document.getElementById("numbers").checked;
 
-for (let i = 0; i < length; i += 1) {
-    const r = generate(0, 3);
+  if (lowerCase + upperCase + symbol + number <= 0) return;
+
+  for (let i = 0; i < length; i += 1) {
+    const r = generater(0, 3);
     if (lowerCase && r === 0) {
       password += generateRandomLowerCase();
     } else if (upperCase && r === 1) {
@@ -19,30 +19,29 @@ for (let i = 0; i < length; i += 1) {
     } else if (symbols && r === 2) {
       password += generateRandomSymbol();
     } else if (numbers && r === 3) {
-      password += generate(0, 9)
+      password += generater(0, 9);
     } else {
       i -= 1;
     }
   }
-  document.getElementById('result') = password;
+  document.getElementById("result").textContent = password;
 }
 
 generate();
 
-
 function generateRandomLowerCase() {
-  return String.fromCharCode(97, 122);
+  return String.fromCharCode(generater(97, 122));
 }
 
 function generateRandomUpperCase() {
-  return String.fromCharCode(65, 90);
+  return String.fromCharCode(generater(65, 90));
 }
 
 function generateRandomSymbol() {
   const symbol = "~*$%@#^&!?*'-=/,.{}()[]<>";
-  return symbols[generate(0, symbols.length - 1)]
+  return symbols[generater(0, symbols.length - 1)];
 }
 
-function generate() {
-  return Math.floor(Math.random() * (max + 1 - min) + min)
+function generater() {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 }
